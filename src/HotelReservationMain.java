71fad5f64c2bc36fb57df4165729599016dffb69
skrzatuski <skrzatuski01@gmail.com>
2021-04-13 14:04:34 +0200

    Add comments

--- src/HotelReservationMain.java
+++ src/HotelReservationMain.java
@@ -2,6 +2,7 @@ public class HotelReservationMain {
     public static void main(String[] args) {
         ControlPanel c1 = new ControlPanel();
         Database d1 = new Database();
+        /*Main loop*/
         do{
             c1.ChooseMenu();
         }while(true);
