e86eac005d3f93e2812253e3e39ed1d4a4eb47d9
skrzatuski <skrzatuski01@gmail.com>
2021-06-08 14:12:57 +0200

    ADD VALIDATION DATA

--- src/ValidationData/DataCheck.java
+++ src/ValidationData/DataCheck.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class DataCheck {
     public boolean CheckingData(String data){
-        SimpleDateFormat sdfrmt = new SimpleDateFormat("YYYY-mm-dd");
+        SimpleDateFormat sdfrmt = new SimpleDateFormat("yyyy-MM-dd");
         sdfrmt.setLenient(false);
         try{
             Date javaDate = sdfrmt.parse(data);
