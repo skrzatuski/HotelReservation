395d2d029c88edc6456d7a042ea476186743b793
skrzatuski <skrzatuski01@gmail.com>
2021-06-08 22:57:23 +0200

    zaktualizowanie menu, nie wyrzuca do glownego menu przy zlym wyborze.

--- src/menu/ReservationMenu.java
+++ src/menu/ReservationMenu.java
@@ -6,6 +6,7 @@ import reservation.DeleteReservation;
 import reservation.ShowAllReservation;
 
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ReservationMenu {
     private int choice;
@@ -14,32 +15,40 @@ public class ReservationMenu {
     private CreateReservation createReservation;
     private DeleteReservation deleteReservation;
     DisplayMenu displayMenu;
-    public void chooseMenu(){
+    public void chooseMenu() {
         displayMenu = new DisplayMenu();
-        displayMenu.reservationChooseMenu();
         createReservation = new CreateReservation();
         showAllReservation = new ShowAllReservation();
         deleteReservation = new DeleteReservation();
         scanner = new Scanner(System.in);
-        choice = scanner.nextInt();
-        switch(choice){
-            case 1:
-                displayMenu.displayReservationMenu();
-                createReservation.createReservation1();
-                break;
-            case 2:
-                displayMenu.displayReservationMenu();
-                showAllReservation.showReservation();
-                break;
-            case 3:
-                displayMenu.deleteReservation();
-                deleteReservation.deleteReservation();
-                break;
-            case 0:
-                break;
-        }
-        if (choice != 1 && choice != 2 && choice != 3 && choice !=0) {
-            System.out.println("Zły wybór");
-        }
+        AtomicBoolean reservationMenuStatus = new AtomicBoolean(false);
+        do {
+            displayMenu.reservationChooseMenu();
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    displayMenu.displayReservationMenu();
+                    createReservation.createReservation1();
+                    reservationMenuStatus.set(false);
+                    break;
+                case 2:
+                    displayMenu.displayReservationMenu();
+                    showAllReservation.showReservation();
+                    reservationMenuStatus.set(false);
+                    break;
+                case 3:
+                    displayMenu.deleteReservation();
+                    deleteReservation.deleteReservation();
+                    reservationMenuStatus.set(false);
+                    break;
+                case 0:
+                    reservationMenuStatus.set(false);
+                    break;
+            }
+            if (choice != 1 && choice != 2 && choice != 3 && choice != 0) {
+                System.out.println("Zły wybór");
+                reservationMenuStatus.set(true);
+            }
+        }while(reservationMenuStatus.get());
     }
 }
--- src/menu/RoomMenu.java
+++ src/menu/RoomMenu.java
@@ -5,6 +5,7 @@ import rooms.RoomGenerator;
 import rooms.ShowAllRooms;
 
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class RoomMenu {
     private int choice;
@@ -16,28 +17,37 @@ public class RoomMenu {
         displayMenu = new DisplayMenu();
         roomGenerator = new RoomGenerator();
         showAllRooms = new ShowAllRooms();
-        displayMenu.roomChoseMenu();
         scanner = new Scanner(System.in);
-        choice = scanner.nextInt();
+        AtomicBoolean roomMenuStatus = new AtomicBoolean(false);
+        do {
+            displayMenu.roomChoseMenu();
+            choice = scanner.nextInt();
             switch (choice) {
                 case 1:
                     displayMenu.roomMenu();
                     roomGenerator.createRoom();
+                    roomMenuStatus.set(false);
                     break;
                 case 2:
                     displayMenu.roomMenu();
                     showAllRooms.showAllRooms();
+                    roomMenuStatus.set(false);
                     break;
                 case 3:
+                    roomMenuStatus.set(false);
                     break;
                 case 4:
+                    roomMenuStatus.set(false);
                     break;
                 case 0:
+                    roomMenuStatus.set(false);
                     break;
             }
             if (choice != 1 && choice != 2 && choice != 3 && choice != 4 && choice != 0) {
                 System.out.println("Zły wybór");
+                roomMenuStatus.set(true);
             }
+        }while(roomMenuStatus.get());
         }
     }
 
